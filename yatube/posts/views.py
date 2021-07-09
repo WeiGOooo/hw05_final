9c0da9c309536b1d7c73b10952486e08862a55b9
WeiGO <nimfeya23@yandex.ru>
2021-07-09 11:49:04 +0300

    sprint_6_final_review_fix_2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,8 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.cache import cache_page
-from django.conf import settings
 
 from .forms import CommentForm, PostForm
 from .models import Follow, Group, Post, User
@@ -148,7 +148,5 @@ def profile_follow(request, username):
 @login_required
 def profile_unfollow(request, username):
     author = get_object_or_404(User, username=username)
-    user = request.user
-    if author != user:
-        Follow.objects.filter(author=author, user=request.user).delete()
+    Follow.objects.filter(author=author, user=request.user).delete()
     return redirect('profile', username=username)
